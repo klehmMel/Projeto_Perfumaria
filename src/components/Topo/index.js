1c834ec9273c6d73ebad69a5b79699821bbfad1f
Melissa Klehm <klehm.m@aluno.ifsp.edu.br>
2025-08-07 17:36:54 -0300

    primeiro commit

---
+++ src/components/Topo/index.js
@@ -0,0 +1,18 @@
+export default function Topo() {
+    return (
+        <header>
+            <div>
+                <picture>
+                    <a href=""> <img src='./logo1.jpg'/> </a>
+                </picture>
+
+                <nav>
+                    <a href="#"> Produtos </a>
+                    <a href="#"> Sobre </a>  
+                    <a href="#"> Contatos </a>
+                     
+                </nav>
+            </div>
+        </header>
+    )
+}
